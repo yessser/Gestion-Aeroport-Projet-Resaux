6d5d3fe641cd2d6c25dc6f297ed58e4f1f5b45dc
amine <ahmane.m.amine@gmail.com>
2023-01-14 05:05:44 +0100

    created stations, flights and planes forms

--- src/main/java/com/example/earth3dtest/HelloApplication.java
+++ src/main/java/com/example/earth3dtest/HelloApplication.java
@@ -27,7 +27,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
