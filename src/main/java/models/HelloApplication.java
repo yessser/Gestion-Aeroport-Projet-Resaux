8b17080fec4851ef848213405212f00c5d76cdb4
mrnbouk <86635426+OutLlawz@users.noreply.github.com>
2023-01-14 17:08:27 +0100

    Add files via upload

---
+++ src/main/java/models/HelloApplication.java
@@ -0,0 +1,39 @@
+package com.example.earth3dtest;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.SubScene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class HelloApplication extends Application {
+
+    @FXML
+    private Sphere earth;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
