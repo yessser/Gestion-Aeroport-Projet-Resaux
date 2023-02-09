c7620eafbcbd466cfcc634df62fbdfc7936eb8b2
amine <ahmane.m.amine@gmail.com>
2023-02-09 18:54:34 +0100

    Fix station list item component

--- src/main/java/com/example/earth3dtest/StationComponentController.java
+++ src/main/java/com/example/earth3dtest/StationComponentController.java
@@ -3,6 +3,7 @@ package com.example.earth3dtest;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import models.Position;
@@ -20,6 +21,10 @@ public class StationComponentController extends ListCell<Station> {
     Text stationLon;
     @FXML
     HBox HBOX;
+    @FXML
+    AnchorPane CellPane;
+
+
     private FXMLLoader mLLoader;
     public void setStationName(String stationName){
         this.StationName.setText(stationName);
@@ -33,8 +38,10 @@ public class StationComponentController extends ListCell<Station> {
     protected void updateItem(Station s,boolean empty){
         super.updateItem(s,empty);
         if(empty ||s==null){
+            setStyle("-fx-control-inner-background:  #1b1b1b;");
             setText(null);
             setGraphic(null);
+
         }else {
             if (mLLoader == null) {
                 mLLoader = new FXMLLoader(getClass().getResource("StationComponent.fxml"));
@@ -45,10 +52,13 @@ public class StationComponentController extends ListCell<Station> {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+//                setStyle("-fx-control-inner-background:  #1b1b1b;");
                 setStationName(s.getNameStation());
                 setPosition(s.getPosition());
                 setText(null);
                 setGraphic(HBOX);
+                setGraphic(CellPane);
+
             }
 
 
